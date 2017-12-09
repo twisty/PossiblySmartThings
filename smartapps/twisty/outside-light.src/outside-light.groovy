2a01422a08faed2846485e6d5a563381b7a94bf7
Tim Brayshaw <tim@brayshaw.com>
2017-12-09 21:17:27 +0000

    Fix SmartThings IDE whitespace.

--- smartapps/twisty/outside-light.src/outside-light.groovy
+++ smartapps/twisty/outside-light.src/outside-light.groovy
@@ -12,84 +12,84 @@ definition(
 )
 
 preferences {
-	section("When I arrive..."){
-		input "presences", "capability.presenceSensor", title: "Who?", multiple: true
-	}
-	section("These devices are switched on...") {
-		input "switches", "capability.switch", title: "Choose switches or lights:", multiple: true
-	}
-	section("Turn off again after...") {
-		input "minutesLater", "number", title: "Minutes:", defaultValue: 5
-	}
+   section("When I arrive..."){
+       input "presences", "capability.presenceSensor", title: "Who?", multiple: true
+   }
+   section("These devices are switched on...") {
+       input "switches", "capability.switch", title: "Choose switches or lights:", multiple: true
+   }
+   section("Turn off again after...") {
+       input "minutesLater", "number", title: "Minutes:", defaultValue: 5
+   }
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"
-	init()
+    log.debug "Installed with settings: ${settings}"
+    init()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
-	unsubscribe()
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
     init()
 }
 
 def init() {
-	def switchStates = [:]
-	def switchLevels = [:]
-	for (device in switches) {
+    def switchStates = [:]
+    def switchLevels = [:]
+    for (device in switches) {
         def key = device.getId()
-    	if (device.hasCapability("Switch Level")) {
-        	switchLevels[key] = device.currentValue("level")
+        if (device.hasCapability("Switch Level")) {
+            switchLevels[key] = device.currentValue("level")
         }
         switchStates[key] = device.currentValue("switch")
     }
     state.initialSwitchStates = switchStates
     state.initialSwitchLevels = switchLevels
-	subscribe(presences, "presence", presenceHandler)
+    subscribe(presences, "presence", presenceHandler)
 }
 
 def presenceHandler(evt) {
-	log.debug "presenceHandler $evt.name: $evt.value"
-	if(evt.value == "present") {
-		log.debug "A presence has arrived"
+    log.debug "presenceHandler $evt.name: $evt.value"
+    if(evt.value == "present") {
+        log.debug "A presence has arrived"
         turnOnSwitches()
     }
-	def presenceValue = presences.find{item -> item.currentPresence == "present"}
-	if (!presenceValue) {
-		log.debug "Everyone's away."
-	}
+    def presenceValue = presences.find{item -> item.currentPresence == "present"}
+    if (!presenceValue) {
+        log.debug "Everyone's away."
+    }
 }
 
 def restoreSwitches() {
-	for (device in switches) {
+    for (device in switches) {
         def key = device.getId()
-    	def thisSwitchInitialState = state.initialSwitchStates[key]
+        def thisSwitchInitialState = state.initialSwitchStates[key]
         if (thisSwitchInitialState == "on") {
-			log.debug "Leaving switch on. ${device}"
-			def thisSwitchInitialLevel = state.initialSwitchLevels[key]
+            log.debug "Leaving switch on. ${device}"
+            def thisSwitchInitialLevel = state.initialSwitchLevels[key]
             if (thisSwitchInitialLevel) {
-            	log.debug "Restoring level to: ${thisSwitchInitialLevel}"
-				device.setLevel(thisSwitchInitialLevel)
+                log.debug "Restoring level to: ${thisSwitchInitialLevel}"
+                device.setLevel(thisSwitchInitialLevel)
             }
-		} else {
-			log.debug "Turning switch off. ${device}"
-        	device.off()
+        } else {
+            log.debug "Turning switch off. ${device}"
+            device.off()
         }
-	}
+    }
 }
 
 def turnOnSwitches() {
-	for (device in switches) {
-		log.debug "Turning on switch. ${device}"
-	    device.on()
-		if (device.hasCapability("Switch Level")) {
-        	def level = 100
-	        log.debug "Setting level to: ${level}"
-			device.setLevel(level)
+    for (device in switches) {
+        log.debug "Turning on switch. ${device}"
+        device.on()
+        if (device.hasCapability("Switch Level")) {
+            def level = 100
+            log.debug "Setting level to: ${level}"
+            device.setLevel(level)
         }
     }
-	def delay = minutesLater * 60
-	log.debug "Restoring switches in ${minutesLater} minutes (${delay} seconds)"
-	runIn(delay, restoreSwitches)
+    def delay = minutesLater * 60
+    log.debug "Restoring switches in ${minutesLater} minutes (${delay} seconds)"
+    runIn(delay, restoreSwitches)
 }
